fa69cbcc702ec69d9055e366b6570d490bc88732
sald19 <carl.sald19@gmail.com>
2013-09-14 01:03:39 -0600

    archivo de template

---
+++ public/js/message.js
@@ -0,0 +1,5 @@
+var Template = {}
+Template.message = '<div class="contentMessage">\
+							<div class="avatar" style="background: {{color}}"></div>\
+							<div class="message">{{message}}</div>\
+						</div>';
